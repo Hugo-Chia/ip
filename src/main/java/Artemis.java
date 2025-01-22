fad1c918001f85bda3dacd1110c46804bc0b8ba0
Hugo-Chia <118971703+Hugo-Chia@users.noreply.github.com>
2025-01-22 20:34:22 +0800

    Added echo feature

--- src/main/java/Artemis.java
+++ src/main/java/Artemis.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Artemis {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Artemis\n");
 
         System.out.println("What can I do for you?\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput;
+        do {
+            userInput = scanner.nextLine();
+            System.out.println(userInput);
+        } while (!userInput.equals("bye"));
 
         System.out.println("Bye. Hope to see you again soon!\n");
     }
