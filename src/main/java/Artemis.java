b3be1ad16388d08de4c571579532feb4e1d7f694
Hugo-Chia <118971703+Hugo-Chia@users.noreply.github.com>
2025-01-22 20:23:17 +0800

    Implemented Level-0: Rename, Greet and Exit

---
+++ src/main/java/Artemis.java
@@ -0,0 +1,9 @@
+public class Artemis {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Artemis\n");
+
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
