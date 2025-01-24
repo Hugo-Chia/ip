c59f5624d872c4932ca3e612f7d15d70778c4a30
Hugo-Chia <118971703+Hugo-Chia@users.noreply.github.com>
2025-01-24 19:41:21 +0800

    Improved error handling

--- src/main/java/Artemis.java
+++ src/main/java/Artemis.java
@@ -24,15 +24,39 @@ public class Artemis {
                         System.out.println(i + 1 + "." + task.toString());
                     }
                 } else if (userInput.startsWith(Commands.mark.name())) {
-                    int index = Integer.parseInt(userInput.substring(5)) - 1;
-                    if (index < list.size()) {
+                    if (userInput.length() == 4) {
+                        throw new ArtemisException("You did not choose a task to mark. Please try again!!! :(\n");
+                    }
+
+                    int index;
+                    try {
+                         index = Integer.parseInt(userInput.substring(5)) - 1;
+                    } catch (NumberFormatException e) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    }
+
+                    if (index < 0 || index >= list.size()) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    } else {
                         Task task = list.get(index);
                         task.markAsDone();
                         System.out.println("Nice! I've marked this task as done:\n" + task.toString());
                     }
                 } else if (userInput.startsWith(Commands.unmark.name())) {
-                    int index = Integer.parseInt(userInput.substring(7)) - 1;
-                    if (index < list.size()) {
+                    if (userInput.length() == 6) {
+                        throw new ArtemisException("You did not choose a task to unmark. Please try again!!! :(\n");
+                    }
+
+                    int index;
+                    try {
+                        index = Integer.parseInt(userInput.substring(7)) - 1;
+                    } catch (NumberFormatException e) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    }
+
+                    if (index < 0 || index >= list.size()) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    } else {
                         Task task = list.get(index);
                         task.markAsNotDone();
                         System.out.println("OK, I've marked this task as not done yet:\n" + task.toString());
@@ -50,6 +74,10 @@ public class Artemis {
                     System.out.println("Got it. I've added this task:\n" + todo.toString());
                     System.out.println("Now you have " + Task.getTaskCount() + " tasks in the list.");
                 } else if (userInput.startsWith(Commands.deadline.name())) {
+                    if (userInput.length() == 8) {
+                        throw new ArtemisException("You did not fill up anything for deadline. Please try again!!! :(\n");
+                    }
+
                     String description = userInput.substring(9, userInput.indexOf("/by") - 1);
                     String by = userInput.substring(userInput.indexOf("/by") + 4);
 
@@ -59,6 +87,10 @@ public class Artemis {
                     System.out.println("Got it. I've added this task:\n" + deadline.toString());
                     System.out.println("Now you have " + Task.getTaskCount() + " tasks in the list.");
                 } else if (userInput.startsWith(Commands.event.name())) {
+                    if (userInput.length() == 5) {
+                        throw new ArtemisException("You did not fill up anything for event. Please try again!!! :(\n");
+                    }
+
                     String description = userInput.substring(6, userInput.indexOf("/from") - 1);
                     String from = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") - 1);
                     String to = userInput.substring(userInput.indexOf("/to") + 4);
@@ -69,8 +101,20 @@ public class Artemis {
                     System.out.println("Got it. I've added this task:\n" + event.toString());
                     System.out.println("Now you have " + Task.getTaskCount() + " tasks in the list.");
                 } else if (userInput.startsWith(Commands.delete.name())) {
-                    int index = Integer.parseInt(userInput.substring(7)) - 1;
-                    if (index < list.size()) {
+                    if (userInput.length() == 6) {
+                        throw new ArtemisException("You did not choose a task to delete. Please try again!!! :(\n");
+                    }
+
+                    int index;
+                    try {
+                        index = Integer.parseInt(userInput.substring(7)) - 1;
+                    } catch (NumberFormatException e) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    }
+
+                    if (index < 0 || index >= list.size()) {
+                        throw new ArtemisException("Invalid index. Please try again!!! :(\n");
+                    } else {
                         list.remove(index);
                         Task.reduceTaskCount();
                         System.out.println("Noted. I've removed this task:\n" + list.get(index).toString());
